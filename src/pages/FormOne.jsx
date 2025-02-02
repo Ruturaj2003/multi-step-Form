37d732d02c53538dc3a7d26b13ee630cef8a65b5
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-02-02 19:12:44 +0530

    Set background color

--- src/pages/FormOne.jsx
+++ src/pages/FormOne.jsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from 'react-router-dom';
 const FormOne = () => {
   return (
     <div>
-      {/* */}
+      {/* bread crumb that tells where i currently am and also i can naviagte to any portion of the form by clicking it */}
       <Outlet /> {/* Renders the child routes (General, Address, Billing) */}
     </div>
   );
