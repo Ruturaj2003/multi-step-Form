fa5430b03da93321aba3e0ff357115b0f926d5d3
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-02-02 22:33:41 +0530

    Add BreadCrumb component and style form layout

---
+++ src/components/BreadCrumb.jsx
@@ -0,0 +1,55 @@
+import { ChevronRightSharp } from '@mui/icons-material';
+import { Link, useLocation } from 'react-router-dom';
+
+const BreadCrumb = () => {
+  const location = useLocation();
+
+  // Helper function to determine if the link is active
+  const getLinkClass = (path) =>
+    `text-white ${
+      location.pathname === path
+        ? 'font-bold underline underline-offset-4'
+        : 'font-bold '
+    }`;
+
+  return (
+    <div className="h-12 flex justify-start items-center">
+      <div className="bg-primary rounded-l-xl rounded-r-3xl text-white max-w-[350px] flex-1 px-4 py-2">
+        {/* Breadcrumbs */}
+        <nav>
+          <ul className="flex space-x-4">
+            <li>
+              <Link to="/form-one" className={getLinkClass('/form-one')}>
+                General
+              </Link>
+            </li>
+            <li>
+              <ChevronRightSharp />
+            </li>
+            <li>
+              <Link
+                to="/form-one/address"
+                className={getLinkClass('/form-one/address')}
+              >
+                Address
+              </Link>
+            </li>
+            <li>
+              <ChevronRightSharp />
+            </li>
+            <li>
+              <Link
+                to="/form-one/billing"
+                className={getLinkClass('/form-one/billing')}
+              >
+                Billing
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default BreadCrumb;
--- src/pages/FormOne.jsx
+++ src/pages/FormOne.jsx
@@ -1,43 +1,17 @@
-import { ChevronRightSharp } from '@mui/icons-material';
-import { Outlet, Link, useLocation } from 'react-router-dom';
-useLocation;
+import { Outlet } from 'react-router-dom';
+import BreadCrumb from '../components/BreadCrumb';
+// Import the new BreadCrumb component
+
 const FormOne = () => {
   return (
-    <div className="container">
-      {/* Bread Crumbs */}
-      <div className="h-12 flex justify-center items-center">
-        <div className="bg-primary rounded-l-xl rounded-r-3xl  text-white  max-w-[350px] flex-1 px-4 py-2">
-          {/* Breadcrumbs */}
-          <nav>
-            <ul className="flex space-x-4">
-              <li>
-                <Link to="/form-one" className="text-white">
-                  General
-                </Link>
-              </li>
-              <li>
-                <ChevronRightSharp></ChevronRightSharp>
-              </li>
-              <li>
-                <Link to="/form-one/address" className="text-white">
-                  Address
-                </Link>
-              </li>
-              <li>
-                <ChevronRightSharp></ChevronRightSharp>
-              </li>
-              <li>
-                <Link to="/form-one/billing" className="text-white">
-                  Billing
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
+    <div className="form-container">
+      {/* Form Container */}
+      <div className="form-card">
+        <p className="text-text text-3xl mb-4">Lorem Ipsum orem Ipsum</p>
+        <BreadCrumb></BreadCrumb>
+        <Outlet />
       </div>
-
       {/* Outlet to render nested routes */}
-      <Outlet />
     </div>
   );
 };
